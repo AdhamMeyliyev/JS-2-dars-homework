e7588653e05b6fa1ce78bbe2c275ca1943e192bd
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2023-12-08 20:31:04 +0500

    java script FIZZBUZZ masalasi

---
+++ index.js
@@ -0,0 +1,29 @@
+// let a=+prompt("Yoshingizni kiriting");
+
+// if(a==" "){
+//     prompt("Siz yoshingizni kiritmadingiz yoki siz 18 yoshdamisz. Agar shunday bo'ladigan bo'lsa yoshingizni kiriting va  OKni bosing ");
+// }else if (a<18){
+//     alert("Bu veb-sayt siz uchun taqiqlangan");
+// }else{
+//     alert("Yoshingiz yetarli OK tugmasini bosin");
+// }
+
+// let a=25;
+
+// if(a%3==0 && a%5==0){
+//     console.log("FIZZBUZZ");
+// }else if (a%3==0){
+//     console.log("FIZZ");
+// }else{
+//     console.log("BUZZ");
+// }
+
+// alert( null || 2 || undefined );//2
+// alert( alert(1) || 2 || alert(3) ); //1, 2
+// alert( 1 && null && 2 ) //null
+// alert( alert(1) && alert(2) );//1, undefined
+// alert( null || 2 && 3 || 4 );//3
+// if (-1 || 0) alert( 'first' );//first
+// if (-1 && 0) alert( 'second' );//0
+// if (null || -1 && 1) alert( 'third' );//third+
+
