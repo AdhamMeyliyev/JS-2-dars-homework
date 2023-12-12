6590085dd386a8941718c0431019e42364600b05
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2023-12-12 13:29:47 +0500

    JS 4-dars

--- index.js
+++ index.js
@@ -27,14 +27,129 @@
 // if (-1 && 0) alert( 'second' );//0
 // if (null || -1 && 1) alert( 'third' );//third+
 
-for(let i=2; i<=10; i++){
+// for(let i=2; i<=10; i++){
 
-    console.log(`${i} karra jadvali`)
+//     console.log(`${i} karra jadvali`)
 
-    for(let j=1; j<=10; j++){
+//     for(let j=1; j<=10; j++){
 
-        console.log(`${i} * ${j} = ${i*j}`)
+//         console.log(`${i} * ${j} = ${i*j}`)
 
-    }
+//     }
 
-}
+// }
+
+
+// for (let i=2; i<10; i++){
+//     console.log(`${i} karra `);
+
+//     for(let l=1; l<=10; l++){
+//         console.log(`${i} * ${l} = ${i*l}`);
+//     }
+// }
+
+// let obj={
+//     john: 500,
+//     salim: 400,
+//     alim: 200,
+// }
+
+// console.log(obj.john + obj.salim + obj.alim);
+
+// let obj={
+//         john: 500,
+//         salim: 400,
+//         alim: 200,
+//     }
+
+//     for( let key in obj){
+//         console.log(obj[key]);
+        
+//     }
+
+
+// JS 4-DARS HOMEWORK 1
+// function calcMin(a=10, b=-1){
+//     if(a>b)
+//         console.log(b);
+//     else
+//         console.log(a);
+
+//         return calcMin;
+// }
+
+// console.log(calcMin());
+
+
+
+
+// JS 4-DARS HOMEWORK 2
+
+// function calcPow(a=3){
+//     let result = a*a;
+//     console.log(result);
+   
+// }
+
+// calcPow();
+
+
+// JS 4-DARS HOMEWORK 3
+
+// let salaries = {
+//     Ann:200,
+//     John:400,
+//     Albert:500,
+//     Jessica:100,
+//     }
+
+
+//     let a=0;
+
+//     for(let key in salaries){
+//         a +=salaries[key];
+//     }
+
+//     console.log(a);
+    
+    
+////////////////////////////////////////////////////////////////////////////////////
+// let arr = ["Apple", "Orange", "Pear"];
+// for (let key in arr) {
+// alert( arr[key] ); // Apple, Orange, Pear
+// }
+
+// let manzil = {
+//     x: 10,
+//     y: 20,
+//     z: 'text', // bu son emas
+//     a: 5,
+//   };
+
+// function sonlarniQosh(obyekt) {
+//     let yigindi = 0;
+  
+//     for (let key in obyekt) {
+//       if (typeof obyekt[key] === 'number') {
+//         yigindi += obyekt[key];
+//       }
+//       else{
+//         console.log(undefined);
+//       }
+//     }
+  
+//     return yigindi;
+//   }
+  
+  // Misol uchun obyekt
+
+
+// let a=1;
+// let b=2;
+
+// let result = a+b;
+
+// console.log(result);
+
+// console.log(`${a} + ${b} = ${a+b}`);
+ 
